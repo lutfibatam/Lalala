633cf5e297baa1af21edd913e4d9ff94d49b71a6
taufiqhida <taufiqh2003@gmail.com>
2022-11-29 11:44:13 +0700

    table

--- index.php
+++ index.php
@@ -49,6 +49,36 @@
 <div class="container">
 
   <h1>Belajar Remote</h1>
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Kelas</th>
+      <th scope="col">Bagian</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>Taufiq</td>
+      <td>R</td>
+      <td>Back</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Lutfi</td>
+      <td>L</td>
+      <td>Frot</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td >Bintang</td>
+      <td>L</td>
+      <td>Full stack</td>
+    </tr>
+  </tbody>
+</table>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
