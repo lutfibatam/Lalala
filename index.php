3e021b4e25c3dffb206a756c90fe84df6bc4ad3d
taufiqhida <taufiqh2003@gmail.com>
2022-11-29 11:36:47 +0700

    up

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <div class="container">
 
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <!-- <a class="navbar-brand" href="#">Navbar</a> -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -46,7 +46,10 @@
   </div>
 </div>
 </nav>
-<h1>Belajar Remote</h1>
+<div class="container">
+
+  <h1>Belajar Remote</h1>
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
